097b3bd0dbc57621e9ff5bd7fe13457b8b0b1256
Andres Fernando <andres.fernando08@gmail.com>
2020-07-19 16:16:14 -0500

    changed alternative of img by passing twitter screen_name as a prop

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -13,7 +13,7 @@ class Badge extends React.Component {
                 </div>
 
                 <div className='Badge__section-name'>
-                    <img className='Badge__avatar' src={this.props.avatarUrl} alt="Avatar" />
+                    <img className='Badge__avatar' src={this.props.avatarUrl} alt={this.props.twitter} />
                     <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
                 </div>
 
