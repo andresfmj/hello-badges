951b11ae546d212abd107c201923cd88e254800e
Andres Fernando <andres.fernando08@gmail.com>
2020-07-22 17:00:53 -0500

    simulate errors from fake api

--- src/api.js
+++ src/api.js
@@ -24,10 +24,11 @@ async function callApi(endpoint, options = {}) {
 const api = {
   badges: {
     list() {
-      throw new Error('Not Found')
-      // return callApi('/badges');
+      // throw new Error('500: Internal Server Error')
+      return callApi('/badges');
     },
     create(badge) {
+      // throw new Error('500: Internal Server Error')
       return callApi(`/badges`, {
         method: 'POST',
         body: JSON.stringify(badge),
