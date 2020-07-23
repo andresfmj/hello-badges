e71b9e9e05bbfc7510f90f186290498d223ad694
Andres Fernando <andres.fernando08@gmail.com>
2020-07-22 23:31:25 -0500

    learning how hooks is working

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -7,8 +7,17 @@ import confLogo from '../images/platziconf-logo.svg'
 import Badge from '../components/Badge'
 import DeleteBadgeModal from '../components/DeleteBadgeModal'
 
+function useIncreaseCount(max) {
+    const [count, setCount] = React.useState(0)
+    if (count > max) {
+        setCount(0)
+    }
+    return [count, setCount]
+}
 
 function BadgeDetails(props) {
+
+    const [count, setCount] = useIncreaseCount(7)
 
     const badge = props.badge
 
@@ -36,7 +45,10 @@ function BadgeDetails(props) {
                     <div className="col">
                         <h2>Actions</h2>
                         <div>
-                            <div><Link className='btn btn-primary mb-4' to={`/badges/${badge.id}/edit`}>Edit</Link></div>
+                            <div>
+                                <button onClick={() => setCount(count + 1)} className='btn btn-primary mr-4'>Increase Count {count}</button>
+                                <Link className='btn btn-primary mb-4' to={`/badges/${badge.id}/edit`}>Edit</Link>
+                            </div>
                         </div>
                         <div>
                             <button onClick={props.toggleModal} className='btn btn-danger'>Delete</button>
